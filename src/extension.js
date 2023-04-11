caf492f3405fb0dad0d1d13ed9290b8df1b76694
Xuangeaha <xiangzhenxuan@163.com>
2023-04-11 22:02:28 +0800

    1.0.0b

--- src/extension.js
+++ src/extension.js
@@ -9,8 +9,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            snippet = new vscode.SnippetString("[![Version](https://vsmarketplacebadges.dev/version/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/version/${1:publishername.extensionname}.svg\" alt=\"Version\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
@@ -23,8 +33,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            snippet = new vscode.SnippetString("[![Version Short](https://vsmarketplacebadges.dev/version-short/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/version-short/${1:publishername.extensionname}.svg\" alt=\"Version Short\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
@@ -37,8 +57,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            snippet = new vscode.SnippetString("[![Installs](https://vsmarketplacebadges.dev/installs/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/installs/${1:publishername.extensionname}.svg\" alt=\"Installs\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
@@ -51,8 +81,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            snippet = new vscode.SnippetString("[![Installs Short](https://vsmarketplacebadges.dev/installs-short/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/installs-short/${1:publishername.extensionname}.svg\" alt=\"Installs Short\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
@@ -65,8 +105,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            snippet = new vscode.SnippetString("[![Downloads](https://vsmarketplacebadges.dev/downloads/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/downloads/${1:publishername.extensionname}.svg\" alt=\"Downloads\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
@@ -79,8 +129,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            snippet = new vscode.SnippetString("[![Downloads Short](https://vsmarketplacebadges.dev/downloads-short/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/downloads-short/${1:publishername.extensionname}.svg\" alt=\"Downloads Short\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
@@ -93,8 +153,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            snippet = new vscode.SnippetString("[![Rating](https://vsmarketplacebadges.dev/rating/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/rating/${1:publishername.extensionname}.svg\" alt=\"Rating\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
@@ -107,8 +177,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            snippet = new vscode.SnippetString("[![Rating Short](https://vsmarketplacebadges.dev/rating-short/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/rating-short/${1:publishername.extensionname}.svg\" alt=\"Rating\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
@@ -121,8 +201,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            snippet = new vscode.SnippetString("[![Rating Star](https://vsmarketplacebadges.dev/rating-star/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/rating-star/${1:publishername.extensionname}.svg\" alt=\"Rating Short\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
@@ -135,8 +225,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            const snippet = new vscode.SnippetString("[![TrendingDaily](https://vsmarketplacebadges.dev/trending-daily/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            const snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/trending-daily/${1:publishername.extensionname}.svg\" alt=\"TrendingDaily\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
@@ -149,8 +249,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            snippet = new vscode.SnippetString("[![TrendingWeekly](https://vsmarketplacebadges.dev/trending-weekly/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/trending-weekly/${1:publishername.extensionname}.svg\" alt=\"TrendingWeekly\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
@@ -163,8 +273,18 @@ function activate(context) {
             vscode.window.showErrorMessage('No active editor found');
             return;
         }
+
         // 获取代码片段
-        const snippet = new vscode.SnippetString();
+        const languageId = vscode.window.activeTextEditor?.document.languageId;
+        var snippet;
+
+        if (languageId == 'markdown') {
+            snippet = new vscode.SnippetString("[![TrendingMonthly](https://vsmarketplacebadges.dev/trending-monthly/${1:publishername.extensionname}.svg)](https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname})");
+        } else {
+            snippet = new vscode.SnippetString("<a href=\"https://marketplace.visualstudio.com/items?itemName=${1:publishername.extensionname}\">",
+            "\t<img src=\"https://vsmarketplacebadges.dev/trending-monthly/${1:publishername.extensionname}.svg\" alt=\"TrendingMonthly\">",
+            "</a>");
+        }
 
         // 插入代码片段
         editor.insertSnippet(snippet);
